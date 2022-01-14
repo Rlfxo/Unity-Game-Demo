ca989a6a65de6d747d1d109b7c8b80a9c35f95dd
yskl7137 <yskl7137@gmail.com>
2022-01-14 16:34:27 +0900

    Player X-Y move animation

--- Rlfxo2048Demo/Assets/Scripts/MoveController.cs
+++ Rlfxo2048Demo/Assets/Scripts/MoveController.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class MoveController : MonoBehaviour
 {
-    public float movementSpeed = 3.0f;
+    public float movementSpeed = 1.5f;
     Vector2 movement = new Vector2();
     bool isHorizonMove;
     Rigidbody2D rigidbody2D;
+    Animator anim;
     // Start is called before the first frame update
     void Start(){
+        anim = GetComponent<Animator>();
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
@@ -18,14 +20,25 @@ public class MoveController : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         
-        if(Input.GetButtonDown("Horizontal") || Input.GetButtonUp("Vertical")) isHorizonMove = true;
-        else if(Input.GetButtonDown("Vertical") || Input.GetButtonUp("Horizontal")) isHorizonMove = false;
+        if(Input.GetButtonDown("Horizontal")) isHorizonMove = true;
+        else if(Input.GetButtonDown("Vertical")) isHorizonMove = false;
+        else if(Input.GetButtonUp("Horizontal") || Input.GetButtonUp("Vertical")) isHorizonMove = movement.x != 0;
+
+        if(anim.GetInteger("hAxisRaw") != (int)movement.x){
+            anim.SetBool("isMove", true);
+            anim.SetInteger("hAxisRaw", (int)movement.x);
+            
+        }else if(anim.GetInteger("vAxisRaw") != (int)movement.y){
+            anim.SetBool("isMove", true);
+            anim.SetInteger("vAxisRaw", (int)movement.y);
+            
+        }else anim.SetBool("isMove", false);
     }
 
     private void FixedUpdate() {
         if(isHorizonMove) movement.y = 0;
         else movement.x = 0;
-        
+
         movement.Normalize();
         rigidbody2D.velocity = movement * movementSpeed;
     }
