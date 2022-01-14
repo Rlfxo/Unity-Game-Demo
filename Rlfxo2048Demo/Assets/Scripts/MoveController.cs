23d72b00133313597e4bef36e07fac46942a24a4
yskl7137 <yskl7137@gmail.com>
2022-01-14 11:45:16 +0900

    Move Controller be X-Y move

--- Rlfxo2048Demo/Assets/Scripts/MoveController.cs
+++ Rlfxo2048Demo/Assets/Scripts/MoveController.cs
@@ -6,6 +6,7 @@ public class MoveController : MonoBehaviour
 {
     public float movementSpeed = 3.0f;
     Vector2 movement = new Vector2();
+    bool isHorizonMove;
     Rigidbody2D rigidbody2D;
     // Start is called before the first frame update
     void Start(){
@@ -14,15 +15,18 @@ public class MoveController : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
         
+        if(Input.GetButtonDown("Horizontal") || Input.GetButtonUp("Vertical")) isHorizonMove = true;
+        else if(Input.GetButtonDown("Vertical") || Input.GetButtonUp("Horizontal")) isHorizonMove = false;
     }
 
     private void FixedUpdate() {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-
+        if(isHorizonMove) movement.y = 0;
+        else movement.x = 0;
+        
         movement.Normalize();
-
         rigidbody2D.velocity = movement * movementSpeed;
     }
 }
