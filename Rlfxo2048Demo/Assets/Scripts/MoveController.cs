39faf15b6e41058285ad8a2db6456363ff4d3670
yskl7137 <yskl7137@gmail.com>
2022-01-14 18:59:18 +0900

    Player Ray + NPC

--- Rlfxo2048Demo/Assets/Scripts/MoveController.cs
+++ Rlfxo2048Demo/Assets/Scripts/MoveController.cs
@@ -7,6 +7,8 @@ public class MoveController : MonoBehaviour
     public float movementSpeed = 1.5f;
     Vector2 movement = new Vector2();
     bool isHorizonMove;
+    Vector3 dirVec;
+    GameObject scanObject;
     Rigidbody2D rigidbody2D;
     Animator anim;
     // Start is called before the first frame update
@@ -17,13 +19,16 @@ public class MoveController : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
+        // movement
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         
+        // XY movement
         if(Input.GetButtonDown("Horizontal")) isHorizonMove = true;
         else if(Input.GetButtonDown("Vertical")) isHorizonMove = false;
         else if(Input.GetButtonUp("Horizontal") || Input.GetButtonUp("Vertical")) isHorizonMove = movement.x != 0;
 
+        // Animation
         if(anim.GetInteger("hAxisRaw") != (int)movement.x){
             anim.SetBool("isMove", true);
             anim.SetInteger("hAxisRaw", (int)movement.x);
@@ -33,13 +38,29 @@ public class MoveController : MonoBehaviour
             anim.SetInteger("vAxisRaw", (int)movement.y);
             
         }else anim.SetBool("isMove", false);
+
+        // Get direction
+        if (Input.GetButtonDown("Vertical") && movement.y == 1) dirVec = Vector3.up;
+        else if(Input.GetButtonDown("Vertical") && movement.y == -1) dirVec = Vector3.down;
+        else if(Input.GetButtonDown("Horizontal") && movement.x == -1) dirVec = Vector3.left;
+        else if(Input.GetButtonDown("Horizontal") && movement.x == 1) dirVec = Vector3.right;
+
+        // Scan Object
+        if(Input.GetButtonDown("Jump") && scanObject != null) Debug.Log("Object" + scanObject.name);
     }
 
     private void FixedUpdate() {
         if(isHorizonMove) movement.y = 0;
         else movement.x = 0;
-
+        // Move
         movement.Normalize();
         rigidbody2D.velocity = movement * movementSpeed;
+
+        // Ray
+        Debug.DrawRay(rigidbody2D.position, dirVec * 0.6f, new Color(0,1,0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rigidbody2D.position, dirVec, 0.6f, LayerMask.GetMask("Object"));
+        if(rayHit.collider != null) {
+            scanObject = rayHit.collider.gameObject;
+        }else scanObject = null;
     }
 }
