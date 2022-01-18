890fa81622aa5951ab7a72737b5aaa32cdebfe18
yskl7137 <yskl7137@gmail.com>
2022-01-18 17:57:03 +0900

    Player talk -> isNotMove

--- Rlfxo2048Demo/Assets/Scripts/MoveController.cs
+++ Rlfxo2048Demo/Assets/Scripts/MoveController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MoveController : MonoBehaviour
 {
     public float movementSpeed = 1.5f;
+    public GameManager manager;
     Vector2 movement = new Vector2();
     bool isHorizonMove;
     Vector3 dirVec;
@@ -20,8 +21,8 @@ public class MoveController : MonoBehaviour
     // Update is called once per frame
     void Update(){
         // movement
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        movement.x = manager.isAction ? 0 : Input.GetAxisRaw("Horizontal");
+        movement.y = manager.isAction ? 0 : Input.GetAxisRaw("Vertical");
         
         // XY movement
         if(Input.GetButtonDown("Horizontal")) isHorizonMove = true;
@@ -46,7 +47,8 @@ public class MoveController : MonoBehaviour
         else if(Input.GetButtonDown("Horizontal") && movement.x == 1) dirVec = Vector3.right;
 
         // Scan Object
-        if(Input.GetButtonDown("Jump") && scanObject != null) Debug.Log("Object" + scanObject.name);
+        if(Input.GetButtonDown("Jump") && scanObject != null) manager.Action(scanObject);
+        //Debug.Log("Object" + scanObject.name);
     }
 
     private void FixedUpdate() {
@@ -57,7 +59,7 @@ public class MoveController : MonoBehaviour
         rigidbody2D.velocity = movement * movementSpeed;
 
         // Ray
-        Debug.DrawRay(rigidbody2D.position, dirVec * 0.6f, new Color(0,1,0));
+        //Debug.DrawRay(rigidbody2D.position, dirVec * 0.6f, new Color(0,1,0));
         RaycastHit2D rayHit = Physics2D.Raycast(rigidbody2D.position, dirVec, 0.6f, LayerMask.GetMask("Object"));
         if(rayHit.collider != null) {
             scanObject = rayHit.collider.gameObject;
