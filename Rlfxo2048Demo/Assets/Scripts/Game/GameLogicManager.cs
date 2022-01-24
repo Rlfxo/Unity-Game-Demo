d4f9170358ad40f7ac594def85b7c66e2b5709c3
yskl7137 <yskl7137@gmail.com>
2022-01-24 17:33:24 +0900

    Bolck Merge and Combine tag

--- Rlfxo2048Demo/Assets/Scripts/Game/GameLogicManager.cs
+++ Rlfxo2048Demo/Assets/Scripts/Game/GameLogicManager.cs
@@ -9,7 +9,8 @@ public class GameLogicManager : MonoBehaviour{
 
     bool inputWait, moveWait;
 
-    int x, y, i;
+    int x, y, i;// Block Move
+    int j;// Monster Level
     int gridSize = 5;
     Vector3 firstPos, gap;
     GameObject[,] Grid = new GameObject[5, 5];
@@ -32,16 +33,37 @@ public class GameLogicManager : MonoBehaviour{
 
             if(inputWait) {
                 inputWait = false;
-                //Up
-                if(gap.y > 0 && gap.x > -0.5f && gap.x < 0.5f) { Debug.Log("Up"); }
-                //Down
-                else if(gap.y < 0 && gap.x > -0.5f && gap.x < 0.5f) { }
-                //Right
-                else if(gap.x > 0 && gap.y > -0.5f && gap.y < 0.5f) { }
-                //Left
-                else if(gap.x < 0 && gap.y > -0.5f && gap.y < 0.5f){
+
+                if(gap.y > 0 && gap.x > -0.5f && gap.x < 0.5f){// Up
+                    for(x = 0; x < gridSize; x++){
+                        for(y = 0; y < gridSize-1; y++){
+                            for(i = gridSize-1; i > y; i--){
+                                MoveOrMerge(x, i - 1, x, i);
+                            }
+                        }
+                    }
+                }
+                else if(gap.y < 0 && gap.x > -0.5f && gap.x < 0.5f){// Down
+                    for(x = 0; x < gridSize; x++){
+                        for(y = gridSize-1; y > 0; y--){
+                            for(i = 0; i < y; i++){
+                                MoveOrMerge(x, i + 1, x, i);
+                            }
+                        }
+                    }
+                }
+                else if(gap.x > 0 && gap.y > -0.5f && gap.y < 0.5f){// Right
                     for(y = 0; y < gridSize; y++){
-                        for(x = gridSize - 1; x > 0; x--){
+                        for(x = 0; x < gridSize-1; x++){
+                            for(i = gridSize-1; i > x; i--){
+                                MoveOrMerge(i - 1, y, i, y);
+                            }
+                        }
+                    }
+                }
+                else if(gap.x < 0 && gap.y > -0.5f && gap.y < 0.5f){// Left
+                    for(y = 0; y < gridSize; y++){
+                        for(x = gridSize-1; x > 0; x--){
                             for(i = 0; i < x; i++){
                                 MoveOrMerge(i + 1, y, i, y);
                             }
@@ -50,9 +72,16 @@ public class GameLogicManager : MonoBehaviour{
                 }
                 else return;
 
-                if(moveWait) {
+                if(moveWait) {// Move -> Spawn
                     moveWait = false;
                     Spawn();
+
+                    for(x = 0; x < gridSize; x++){
+                        for(y = 0; y < gridSize; y++){
+                            if(Grid[x, y] == null) continue;
+                            if(Grid[x, y].tag == "Combine") Grid[x, y].tag = "Untagged";
+                        }
+                    }
                 }
             }
         }
@@ -63,9 +92,24 @@ public class GameLogicManager : MonoBehaviour{
         if(Grid[x2, y2] == null && Grid[x1, y1] != null){// 현제 좌표에 블럭이 있고 목표 좌표가 비어있을 때
             moveWait = true;
             //Grid[x1, y1].transform.position = Vector3.MoveTowards(Grid[x1, y1].transform.position, new Vector3(1.1f * x2 -2.2f, 1.1f * y2 -2.5f, 0), 10000);
-            Grid[x1, y1].GetComponent<Moving>().Move(x2, y2);
+            Grid[x1, y1].GetComponent<Moving>().Move(x2, y2, false);
             Grid[x2, y2] = Grid[x1, y2];
             Grid[x1, y1] = null;
+        }
+
+        if(Grid[x1, y1] != null && Grid[x2, y2] != null && Grid[x1, y1].name == Grid[x2, y2].name && Grid[x1, y1].tag != "Combine" && Grid[x2, y2].tag != "Combine"){// 현재 좌표의 블럭이 목표 좌표의 블럭과 병합
+            moveWait = true;
+            for(j = 0; j <5; j++){// Monster Level Check
+                if(Grid[x2, y2].name == n[j].name + "(Clone)"){
+                    break;
+                }
+            }
+            Grid[x1, y1].GetComponent<Moving>().Move(x2, y2, true);
+            Destroy(Grid[x2, y2]);
+            Grid[x1, y1] = null;
+            Grid[x2, y2] = Instantiate(n[j+1], new Vector3(1.1f * x2 -2.2f, 1.1f * y2 -2.5f, 0), Quaternion.identity);
+            Grid[x2, y2].tag = "Combine";
+            Grid[x2, y2].GetComponent<Animator>().SetTrigger("Merge");
         }
     }
 
--- Rlfxo2048Demo/Assets/Scripts/Game/Moving.cs
+++ Rlfxo2048Demo/Assets/Scripts/Game/Moving.cs
@@ -2,19 +2,24 @@ using UnityEngine;
 
 public class Moving : MonoBehaviour
 {
-    bool move;
+    bool move, _merge;
     int _x2, _y2;
     void Update(){
-        if(move) Move(_x2, _y2);
+        if(move) Move(_x2, _y2, _merge);
     }
 
-    public void Move(int x2, int y2){
+    public void Move(int x2, int y2, bool merge){
         move = true;
         _x2 = x2;
         _y2 = y2;
+        _merge = merge;
         transform.position = Vector3.MoveTowards(transform.position, new Vector3(1.1f * x2 -2.2f, 1.1f * y2 -2.5f, 0), 0.25f);
         if (transform.position == new Vector3(1.1f * x2 -2.2f, 1.1f * y2 -2.5f, 0)){
             move = false;
+            if(merge){
+                _merge = false;
+                Destroy(gameObject);
+            }
         }
     }
 }
