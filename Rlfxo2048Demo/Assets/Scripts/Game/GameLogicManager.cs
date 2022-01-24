c2bc4ceb441e1fe87f0366e6310ff0d0334d2f08
yskl7137 <yskl7137@gmail.com>
2022-01-24 12:52:03 +0900

    xy Grid Input (Mouse & Touch)

--- Rlfxo2048Demo/Assets/Scripts/Game/GameLogicManager.cs
+++ Rlfxo2048Demo/Assets/Scripts/Game/GameLogicManager.cs
@@ -7,8 +7,11 @@ public class GameLogicManager : MonoBehaviour{
 
     public GameObject[] n;
 
+    bool inputWait;
+
     int x,y;
     int gridSize = 5;
+    Vector3 firstPos, gap;
     GameObject[,] Grid = new GameObject[5, 5];
     void Start(){
         Spawn();
@@ -18,6 +21,28 @@ public class GameLogicManager : MonoBehaviour{
     void Update(){
         // 뒤로가기 -> 종료
         if(Input.GetKeyDown(KeyCode.Escape)) Application.Quit();
+        if(Input.GetMouseButtonDown(0) || (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)){
+            inputWait = true;
+            firstPos = Input.GetMouseButtonDown(0) ? Input.mousePosition : (Vector3)Input.GetTouch(0).position;
+        }
+        if(Input.GetMouseButton(0) || (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved)){
+            gap = (Input.GetMouseButton(0)? Input.mousePosition : (Vector3)Input.GetTouch(0).position) - firstPos;
+            if(gap.magnitude < 100) return;
+            gap.Normalize();
+
+            if(inputWait) {
+                inputWait = false;
+                //Up
+                if(gap.y > 0 && gap.x > -0.5f && gap.x < 0.5f) { Debug.Log("Up"); }
+                //Down
+                else if(gap.y < 0 && gap.x > -0.5f && gap.x < 0.5f) { }
+                //Right
+                else if(gap.x > 0 && gap.y > -0.5f && gap.y < 0.5f) { }
+                //Left
+                else if(gap.x < 0 && gap.y > -0.5f && gap.y < 0.5f) { }
+                else return;
+            }
+        }
     }
 
     void Spawn(){// Object 생성
@@ -26,7 +51,8 @@ public class GameLogicManager : MonoBehaviour{
             y = Random.Range(0,gridSize);
             if(Grid[x, y] == null) break;
         }
-        Grid[x,y] = Instantiate(Random.Range(0,5) > 0? n[0]:n[1], new Vector3(1.1f * x -2.2f, 1.1f * y -2.5f, 0), Quaternion.identity);
+        Grid[x, y] = Instantiate(Random.Range(0,5) > 0? n[0]:n[1], new Vector3(1.1f * x -2.2f, 1.1f * y -2.5f, 0), Quaternion.identity);
+        Grid[x, y].GetComponent<Animator>().SetTrigger("Spawn");
     }
 
     public void Restart(){// 재시작 (ReStart, TryAgain)
