77a8156de222ff17a3a5f45d9fb2c49dec15fab1
yskl7137 <yskl7137@gmail.com>
2022-01-24 12:09:30 +0900

    Monster Spawn Random Grid

--- Rlfxo2048Demo/Assets/Scripts/Game/GameLogicManager.cs
+++ Rlfxo2048Demo/Assets/Scripts/Game/GameLogicManager.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameLogicManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class GameLogicManager : MonoBehaviour{
+
+    public GameObject[] n;
+
+    int x,y;
+    int gridSize = 5;
+    GameObject[,] Grid = new GameObject[5, 5];
+    void Start(){
+        Spawn();
+        Spawn();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        // 뒤로가기 -> 종료
+        if(Input.GetKeyDown(KeyCode.Escape)) Application.Quit();
+    }
+
+    void Spawn(){// Object 생성
+        while(true){
+            x = Random.Range(0,gridSize);
+            y = Random.Range(0,gridSize);
+            if(Grid[x, y] == null) break;
+        }
+        Grid[x,y] = Instantiate(Random.Range(0,5) > 0? n[0]:n[1], new Vector3(1.1f * x -2.2f, 1.1f * y -2.5f, 0), Quaternion.identity);
+    }
+
+    public void Restart(){// 재시작 (ReStart, TryAgain)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
