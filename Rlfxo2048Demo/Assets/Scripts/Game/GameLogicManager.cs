3f0a720e0137c8b882ac9bd3d725006865b51107
yskl7137 <yskl7137@gmail.com>
2022-01-24 16:46:49 +0900

    Block Move Axis

--- Rlfxo2048Demo/Assets/Scripts/Game/GameLogicManager.cs
+++ Rlfxo2048Demo/Assets/Scripts/Game/GameLogicManager.cs
@@ -7,9 +7,9 @@ public class GameLogicManager : MonoBehaviour{
 
     public GameObject[] n;
 
-    bool inputWait;
+    bool inputWait, moveWait;
 
-    int x,y;
+    int x, y, i;
     int gridSize = 5;
     Vector3 firstPos, gap;
     GameObject[,] Grid = new GameObject[5, 5];
@@ -39,9 +39,33 @@ public class GameLogicManager : MonoBehaviour{
                 //Right
                 else if(gap.x > 0 && gap.y > -0.5f && gap.y < 0.5f) { }
                 //Left
-                else if(gap.x < 0 && gap.y > -0.5f && gap.y < 0.5f) { }
+                else if(gap.x < 0 && gap.y > -0.5f && gap.y < 0.5f){
+                    for(y = 0; y < gridSize; y++){
+                        for(x = gridSize - 1; x > 0; x--){
+                            for(i = 0; i < x; i++){
+                                MoveOrMerge(i + 1, y, i, y);
+                            }
+                        }
+                    }
+                }
                 else return;
+
+                if(moveWait) {
+                    moveWait = false;
+                    Spawn();
+                }
             }
+        }
+    }
+
+    void MoveOrMerge(int x1, int y1, int x2, int y2){
+        // [x1, y2] now -> [x2, y2] target
+        if(Grid[x2, y2] == null && Grid[x1, y1] != null){// 현제 좌표에 블럭이 있고 목표 좌표가 비어있을 때
+            moveWait = true;
+            //Grid[x1, y1].transform.position = Vector3.MoveTowards(Grid[x1, y1].transform.position, new Vector3(1.1f * x2 -2.2f, 1.1f * y2 -2.5f, 0), 10000);
+            Grid[x1, y1].GetComponent<Moving>().Move(x2, y2);
+            Grid[x2, y2] = Grid[x1, y2];
+            Grid[x1, y1] = null;
         }
     }
 
--- Rlfxo2048Demo/Assets/Scripts/Game/Moving.cs
+++ Rlfxo2048Demo/Assets/Scripts/Game/Moving.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Moving : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    bool move;
+    int _x2, _y2;
+    void Update(){
+        if(move) Move(_x2, _y2);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Move(int x2, int y2){
+        move = true;
+        _x2 = x2;
+        _y2 = y2;
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(1.1f * x2 -2.2f, 1.1f * y2 -2.5f, 0), 0.25f);
+        if (transform.position == new Vector3(1.1f * x2 -2.2f, 1.1f * y2 -2.5f, 0)){
+            move = false;
+        }
     }
 }
